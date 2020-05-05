32a2e837d23b2dd8933e759a07c338226913ed3a
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-05-05 12:27:29 +0900

    add index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,59 @@
-@extends('layputs.app');
+@extends('layouts.app')
 
 @section('content')
+{{-- container --}}
+<div>
+  container
+</div>
+
+{{-- header --}}
+<header>
+  header
+</header>
+
+{{-- global nav --}}
+<nav>
+  nav bar
+</nav>
+
+{{-- contents --}}
+<div>
+  contents
+</div>
+
+{{-- main --}}
+<main>
+  main page
+</main>
+
+{{-- article --}}
+<article>
+  article
+</article>
+
+{{-- aside --}}
+<aside>
+  aside
+</aside>
+
+{{-- information --}}
+<div>
+  info
+</div>
+
+{{-- topics --}}
+<div>
+  topics
+</div>
+
+{{-- news --}}
+<div>
+  news
+</div>
+
+{{-- footer --}}
+<div>
+  footert
+</div>
 
 @endsection
