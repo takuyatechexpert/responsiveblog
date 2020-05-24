b03dd2cd3e9c451c4c720ed9a47be329bab5f735
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-05-24 16:40:10 +0900

    fix index view loop
    投稿、ニュースの表示回数をloop処理で制限実装

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,18 +38,21 @@
       {{-- article --}}
       <article class="contents__item">
         @foreach ($posts as $post)
-          <h2>{{$post->title}}</h2>
-          <div class="contents__item__box">
-            {{-- public/imageに格納したアプリに必要な画像を取り出す方法 --}}
-            {{-- <img src="{{ asset('/images/sample.jpg') }}" alt="イメージ写真"> --}}
-
-            {{-- 投稿した画像を取り出す方法 storage/publicにアクセスしている --}}
-            <img src="{{ asset('storage/' . $post->image) }}" alt="投稿画像">
-            <p>
-              {{$post->description}}
+          @if ($loop->index < 3)
+          {{-- loop処理を使うとforeachでkeyを指定しなくともindexが取れる --}}
+            <h2>{{$post->title}}</h2>
+            <div class="contents__item__box">
+              {{-- public/imageに格納したアプリに必要な画像を取り出す方法 --}}
+              {{-- <img src="{{ asset('/images/sample.jpg') }}" alt="イメージ写真"> --}}
               
-            </p>
-          </div>
+              {{-- 投稿した画像を取り出す方法 storage/publicにアクセスしている --}}
+              <img src="{{ asset('storage/' . $post->image) }}" alt="投稿画像">
+              <p>
+                {{$post->description}}
+                
+              </p>
+            </div>
+          @endif
         @endforeach
       </article>
     </main>
@@ -66,12 +69,14 @@
 
         <ul class="info__guide">
           @foreach ($posts as $post)
-            <li>
-              <img src="{{ asset('storage/' . $post->image) }}" alt="投稿画像">
-              <div class="info__guide__text">
-                <p>{{$post->title}}</p>
-              </div>
-            </li>
+            @if ($loop->index < 5)
+              <li>
+                <img src="{{ asset('storage/' . $post->image) }}" alt="投稿画像">
+                <div class="info__guide__text">
+                  <p>{{$post->title}}</p>
+                </div>
+              </li>
+            @endif
           @endforeach
         </ul>
       </section>
@@ -97,12 +102,15 @@
     <section class="news">
       <h2>News</h2>
       @foreach($news as $new)
-        <article class="news__item">
-          <h3>{{ $new->title }}</h3>
-          <p>{{ $new->updated_at->format('Y/m/d G:i') }}</p>
-          {{-- 日付表示を変更している --}}
-          <p>{{ $new->description }}</p>
-        </article>
+        @if ($loop->index < 5)
+
+          <article class="news__item">
+            <h3>{{ $new->title }}</h3>
+            <p>{{ $new->updated_at->format('Y/m/d G:i') }}</p>
+            {{-- 日付表示を変更している --}}
+            <p>{{ $new->description }}</p>
+          </article>
+        @endif
       @endforeach
     </section>
   </div>
