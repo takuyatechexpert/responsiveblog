d0abcdd65b865569d5acabfa2306b8aaf9ca4a00
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-06-20 15:54:43 +0900

    add column genres table and fix table
    ジャンルテーブルにポストテーブルと紐づける外部キーを設定
    それに伴い各テーブルのidのデータ型を修正

--- database/migrations/2020_06_19_134445_create_genres_table.php
+++ database/migrations/2020_06_19_134445_create_genres_table.php
@@ -14,7 +14,7 @@ class CreateGenresTable extends Migration
     public function up()
     {
         Schema::create('genres', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('title');
             $table->timestamps();
         });
