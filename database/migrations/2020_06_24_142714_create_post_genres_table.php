670cdd34aecc4da95f570430497aaff55b026b7b
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-06-24 14:28:25 +0900

    add PostGenre model and migration file

---
+++ database/migrations/2020_06_24_142714_create_post_genres_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_genres', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_genres');
+    }
+}
